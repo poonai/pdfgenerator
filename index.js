203a842941c04ede67e8d082abff14fc683758cf
captain meow <rbalajis25@gmail.com>
2021-05-21 17:03:26 +0530

    initial commit
    Signed-off-by: captain meow <rbalajis25@gmail.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const app = express();
+app.use(express.json())
+app.post("/pdf", async (req, res) => {
+    let bufferObj = Buffer.from(req.body.html, "base64");
+    let decodedString = bufferObj.toString("utf8");
+    const browser = await puppeteer.launch({ headless: true,defaultViewport: {width:1025, height:900} });
+    const page = await browser.newPage();
+    await page.setViewport({width:1025, height:900});
+    await page.goto(`data:text/html,${decodedString}`, {waitUntil: 'networkidle0'});
+    const pdf = await page.pdf({width:'1025px', height:'1600px'});
+    await browser.close();
+    res.contentType("application/pdf");
+    res.send(pdf);
+})
+
+app.listen(3000, () => {
+    console.log("Server started");
+});
