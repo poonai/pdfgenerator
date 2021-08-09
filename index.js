308463a37fb64ea782bfc939976f69b3570bdc50
poonai <rbalajis25@gmail.com>
2021-08-09 14:35:53 +0530

    fix resolution
    Signed-off-by: poonai <rbalajis25@gmail.com>

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ app.use(express.json())
 app.post("/pdf", async (req, res) => {
     let bufferObj = Buffer.from(req.body.html, "base64");
     let decodedString = bufferObj.toString("utf8");
-    const browser = await puppeteer.launch({ headless: true,defaultViewport: {width:1025, height:900} });
+    const browser = await puppeteer.launch({ headless: true,defaultViewport: {width:1370, height:900}, args:["--no-sandbox"]});
     const page = await browser.newPage();
-    await page.setViewport({width:1025, height:900});
+    await page.setViewport({width:1370, height:900});
     await page.goto(`data:text/html,${decodedString}`, {waitUntil: 'networkidle0'});
-    const pdf = await page.pdf({width:'1025px', height:'1600px'});
+    const pdf = await page.pdf({width:'1370px', height:'1600px'});
     await browser.close();
     res.contentType("application/pdf");
     res.send(pdf);
